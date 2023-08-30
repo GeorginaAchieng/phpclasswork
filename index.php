9e0b25bd1c8a104098a7f77f4007fd6bae2c8b6f
GeorginaAchieng <georginaodhiambo@yahoo.com>
2023-08-30 11:36:46 +0300

    my first php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//echo "My first php";
+//line comment;
+/* block comment
+*/
+//Varriables declared using $
+$first_name ="Georgina";
+$last_name = "Achieng";
+
+echo "My name is $first_name  $last_name"
+?>
